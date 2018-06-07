6878dbe5cbf20c95bc6c40a53722a17b780e1216
Kalaneee <33511688+Kalaneee@users.noreply.github.com>
2018-06-07 12:23:26 +0200

    Add config for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # ActionCable
+  config.web_socket_server_url = "wss://kln-chat-1.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ['https://kln-chat-1.herokuapp.com', 'http://kln-chat-1.herokuapp.com']
 end
